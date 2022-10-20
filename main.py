95c72ee1af9a6acedbfb376f59cf282ed77e60d5
thiagormarinho <116230448+ThiagoRMarinho@users.noreply.github.com>
2022-10-20 00:53:01 +0000

    Aprendi a criar variáveis e exibi-las na tela.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #Meu primeiro projeto Python!!!
 # print() = comnado  de saida
 print("Alo Mundo!")
+
+#Quando quiser guardar uma String! (frase)
+nome = "Thiago Marinho"
+#Quando quiser guardar um numero inteiro
+idade = 19
+
+#Exibir o meu nome (que está dentro da variavel nome)
+print(nome, idade)
+
